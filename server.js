0cb463743f105d9f8c9b44dbe5472683a2bf1578
Vitorgabriel5 <156606648+Vitorgabriel5@users.noreply.github.com>
2024-11-21 12:19:40 -0300

    imersao back-end

---
+++ server.js
@@ -0,0 +1,12 @@
+    import express from "express";
+    import routes from "./src/routes/postsRoutes.js";
+    // Importa o framework Express, que é a base para criarmos nossa aplicação web.
+
+    const app = express();
+    // Cria uma instância do Express, que será o nosso servidor web.
+    routes(app);
+
+    app.listen(3000, () => {
+    console.log("Servidor escutando...");
+    });
+// Inicia o servidor, escutando na porta 3000. Quando o servidor estiver pronto, a mensagem "Servidor escutando..." será exibida no console.
